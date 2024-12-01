f509e060e27fc0076b63c0c36a9ef5b5be762b8d
JaqiKal <jaqika@gmail.com>
2024-12-01 11:19:50 +0100

    feat: Implement sticky navbar with scroll behavior
    Navbar hides on scroll down and reappears on scroll up
    
    Utilizes React state and scroll events for dynamic behavior

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,40 +6,61 @@
  *
  * Features:
  * - Fixed at the top of the page (sticky).
+ * - Disappears on scroll down and reappears on scroll up for better visibility.
  * - Responsive navigation with links to different sections of the portfolio.
  * - Hidden name for screen readers.
+ * - Utilizes React state and scroll events to manage visibility dynamically.
  *
  * Author: JaqiKal
  * Date: Nov 2024
  */
 
-import React from "react";
-// Import the logo from the assets folder
+import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.webp";
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        setShowNav(false); // Hide navbar when scrolling down
+      } else {
+        setShowNav(true); // Show navbar when scrolling up
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    // Fixed header with background and padding
-    <header className="bg-transparent text-white p-4 sticky top-0 z-50 shadow-custom-colored backdrop-blur-sm shadow-[0px_0px_8px_rgba(250,2455,255,0.1)]">
+    <header
+      className={`${
+        showNav ? "translate-y-0" : "-translate-y-full"
+      } bg-transparent text-white p-4 sticky top-0 z-50 shadow-custom-colored backdrop-blur-sm shadow-[0px_0px_8px_rgba(250,2455,255,0.1)] transition-transform duration-300`}
+    >
       <div className="container mx-auto flex justify-between items-center">
-        {/* Logo and Name */}
         <div className="flex items-center space-x-3">
-          {/* Logo */}
           <a
             href="/code-cargo/"
             aria-label="Go to homepage"
-            className="group" // Group for hover effects
+            className="group"
           >
             <img
-              src={logo} // Use the imported logo
+              src={logo}
               alt="Logo"
-              className="rounded-full h-14 w-14 transform transition-transform duration-300 ease-in-out group-hover:animate-tilt" // Custom animation on hover
+              className="rounded-full h-14 w-14 transform transition-transform duration-300 ease-in-out group-hover:animate-tilt"
             />
           </a>
-          {/* Visually hidden name */}
           <h1 className="sr-only">Jaqi</h1>
         </div>
-        {/* Navigation links */}
         <nav>
           <ul className="flex space-x-4">
             <li>
